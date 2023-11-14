282a0da3c374febe34ee8c636efa2b71b8fd81cd
smarttech#2023 <rezwanislam857@gmail.com>
2023-11-13 20:38:42 -0500

    Add git

---
+++ src/main/java/com/Generic/Gitgit.java
@@ -0,0 +1,12 @@
+package com.Generic;
+
+public class Gitgit {
+
+	public static void getGit() {
+		System.out.println("Git git");
+	}
+	
+	public static void main(String[] args) {
+		getGit();
+	}
+}
